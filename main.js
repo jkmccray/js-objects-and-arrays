208527b147a9e7a3802b34e3fe0181fe642ae9d5
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-11 15:14:06 -0500

    Completed practice exercise on Paul McCartney

--- main.js
+++ main.js
@@ -101,4 +101,46 @@ console.log(`Part-time instructors: ${nashvilleSoftwareSchool.instructors.partTi
 console.log(`Full-time instructors: ${nashvilleSoftwareSchool.instructors.fullTime[4]}`);
 
 
+// ------- Practice Exercise -------
+// Output the following value to the console:
+// Paul McCartney was in the Beatles from 1960 to 1970. He was born in 1942. He contributed heavily to the Magical Myster Tour Album.
 
+const beatles = {
+  albums: ["Abbey Road", "Sgt Peppers Lonely Heart's Club Band", "Revolver", "Magical Mystery Tour", "Something New"],
+  history: {
+      formed: 1960,
+      disbanded: 1970
+  },
+  members: [
+      {
+          name: "George Harrison",
+          birth: 1943,
+          death: 2001
+      },
+      {
+          name: "Paul McCartney",
+          birth: 1942,
+          death: null
+      },
+      {
+          name: "John Lennon",
+          birth: 1940,
+          death: 1980
+      },
+      {
+          name: "Ringo Starr",
+          birth: 1940,
+          death: null
+      }
+  ]
+}
+
+const who = beatles.members[1].name;
+const beginning = beatles.history.formed;
+const end = beatles.history.disbanded;
+const yearBorn = beatles.members[1].birth;
+const whichAlbum = beatles.albums[3];
+// using concatination: 
+console.log(who + " was in the Beatles from " + beginning + " to " + end + ". He was born in " + yearBorn + ". He contributed heavily to the " + whichAlbum + ".");
+// using template literal:
+console.log(`${who} was in the Beatles from ${beginning} to ${end}. He was born in ${yearBorn}. He contributed heavily to the ${whichAlbum}.`)
